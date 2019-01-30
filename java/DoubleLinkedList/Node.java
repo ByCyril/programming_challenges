8328716cfeb294cf5992266540f46331f985118e
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2019-01-29 18:04:56 -0800

    new program

---
+++ java/DoubleLinkedList/Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+
+	public int id;
+	public Node next;
+	public Node prev;
+	
+	public Node(int id) {
+		this.id = id;
+	}
+
+}
+
---
+++ java/SingleLinkedList/LinkedList.java
@@ -0,0 +1,73 @@
+
+public class LinkedList {
+
+	private Node head;
+	private int size;;
+
+	public void insert(int id) {
+		Node currentHead = head;
+
+		if (currentHead == null) {
+			head = new Node(id);
+			currentHead = head;
+			size += 1;
+		} else {
+			while (currentHead.next != null) {
+				currentHead = currentHead.next;
+			}
+			currentHead.next = new Node(id);
+			size += 1;
+		}
+	}
+
+	public void delete(int index) {
+
+		Node currentHead = head;
+		int i = 0;
+
+		while (i < index - 1) {
+			if (currentHead.next != null) {
+				currentHead = currentHead.next;
+				i += 1;
+			}
+		}
+
+		currentHead.next = currentHead.next.next;
+		size -= 1;
+	}
+
+	public void insertItems(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			int num = nums[i];
+			insert(num);
+		}
+	}
+
+	public int peek() {
+		return head.id;
+	}
+
+	public int size() {
+		return this.size;
+	}
+
+	public void printAll() {
+		Node currentHead = head;
+
+		while (currentHead.next != null) {
+			System.out.println(currentHead.id);
+			currentHead = currentHead.next;
+		}
+
+		System.out.println(currentHead.id);
+	}
+}
+
+
+
+
+
+
+
+
+
